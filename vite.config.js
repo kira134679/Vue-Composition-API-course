83e5606b9e3c9bb94a334d15cdb008c9e0f173db
kira134679 <kira134679@gmail.com>
2024-08-02 17:42:18 +0800

    Change build config.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/Vue-Composition-API-course/',
   plugins: [
     vue(),
     vueDevTools(),
